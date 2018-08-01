58cb20ed80eab327dcbf5e93fa9c3946d521e280
Alessandro Dalfovo <alessandro.dalfovo@paros.srl>
2018-08-01 08:17:51 +0200

    Second case done

--- parser.js
+++ parser.js
@@ -1,7 +1,15 @@
 parse = (inputs) => {
     let object = {}
-    object.foo = true
+    if (inputs.length > 1) {
+        object[asKey(inputs[0])] = inputs[1]
+    } else {
+        object[asKey(inputs[0])] = true
+    }
     return object
+}
+
+asKey = (inputWithLeadingMinus) => {
+    return inputWithLeadingMinus.replace(/-/g, '')
 }
 
 module.exports = parse
--- parser.test.js
+++ parser.test.js
@@ -3,3 +3,7 @@ const parse = require('./parser')
 test('given --foo as input it produces a json object where foo is true', () => {
     expect(parse(['--foo'])).toEqual({foo : true})
 })
+
+test('given "--foo bar" as input it produces a json object where foo is bar', () => {
+    expect(parse(['--foo', 'bar'])).toEqual({foo : "bar"})
+})
